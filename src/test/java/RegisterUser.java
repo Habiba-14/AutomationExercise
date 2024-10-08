0d9cd809f2ebb29f791209e99f9accf73e356e33
habiba <habeba.naser1412@gmail.com>
2024-10-09 00:00:47 +0300

    Second Push

---
+++ src/test/java/RegisterUser.java
@@ -0,0 +1,49 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+
+public class RegisterUser extends Start {
+    @Test
+    public void SignUp() {
+        driver.findElement(By.xpath("//*[@id=\"header\"]/div/div/div/div[2]/div/ul/li[4]/a")).click();
+        driver.findElement(By.name("name")).sendKeys("Habiba");
+        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[3]")).sendKeys("habeba@gmail.com");
+        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/button")).click();
+
+        //enter account info page
+
+        driver.findElement(By.name("title")).click();
+        driver.findElement(By.id("password")).sendKeys("Habiba@2002");
+
+        //selecting from a drop box
+        String[] ids = {"days", "months", "years"};
+        String[] values = {"14", "12", "2002"};
+        for (int i = 0; i < ids.length; i++) {
+            WebElement dropdown = driver.findElement(By.id(ids[i]));
+            Select select = new Select(dropdown);
+            select.selectByValue(values[i]);
+        }
+        //checkbox
+        driver.findElement(By.id("newsletter")).click();
+
+        //fill address info
+        driver.findElement(By.id("first_name")).sendKeys("habiba");
+        driver.findElement(By.id("last_name")).sendKeys("ali");
+        driver.findElement(By.id("address1")).sendKeys("cairo");
+
+        //select country from dropbox
+        WebElement dropdown = driver.findElement(By.id("country"));
+        Select select = new Select(dropdown);
+        select.selectByValue("Australia");
+
+        //fill in the address detailsS
+        driver.findElement(By.id("state")).sendKeys("cairo");
+        driver.findElement(By.id("city")).sendKeys("cairo");
+        driver.findElement(By.id("zipcode")).sendKeys("11111");
+        driver.findElement(By.id("mobile_number")).sendKeys("111110000");
+
+        //click on create account
+        driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div/div/form/button")).click();
+    }
+}
---
+++ src/test/java/Start.java
@@ -0,0 +1,19 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class Start {
+    WebDriver driver;
+    @BeforeTest
+    public void OpenBrowser(){
+        WebDriverManager.chromedriver().setup();
+         driver = new ChromeDriver();
+        driver.get("https://automationexercise.com/");
+    }
+    @AfterTest
+    public void CloseBrowser(){
+        driver.quit();
+    }
+}
